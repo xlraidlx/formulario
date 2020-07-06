31c4332234d742a3a5edc6bcfdcb2aef758fec8e
xlraidlx <67895510+xlraidlx@users.noreply.github.com>
2020-07-06 02:52:41 -0500

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,53 @@
+<?php
+
+$destino= "escribir correo"
+
+$Foto = $_POST["Foto"];
+$Nombre = $_POST["Nombre"];
+$Apellido = $_POST["Apellido"];
+$Cedula = $_POST["Cedula"];
+$Numero = $_POST["Numero"];
+$Edad = $_POST["Edad"];
+$Ciudad = $_POST["Ciudad"];
+$sexo = $_POST["sexo"];
+$sexualidad = $_POST["sexualidad"];
+$Experiencia = $_POST["Experiencia"];
+$Nivel = $_POST["Nivel"];
+$Cursa = $_POST["Cursa"];
+$Estudia = $_POST["Estudia"];
+$Institucion = $_POST["Institucion"];
+$Localizacion = $_POST["Localizacion"];
+$Español = $_POST["Español"];
+$Ingles = $_POST["Ingles"];
+$Frances = $_POST["Frances"];
+$Portugues = $_POST["Portugues"];
+$Idiomas = $_POST["Idiomas"];
+$Ocupacion = $_POST["Ocupacion"];
+$Pareja = $_POST["Pareja"];
+
+$Foto2 = $_POST["Foto2"];
+$Nombre2 = $_POST["Nombre2"];
+$Apellido2 = $_POST["Apellido2"];
+$Cedula2 = $_POST["Cedula2"];
+$Numero2 = $_POST["Numero2"];
+$Edad2 = $_POST["Edad2"];
+$Ciudad2 = $_POST["Ciudad2"];
+$sexo2 = $_POST["sexo2"];
+$sexualidad2 = $_POST["sexualidad2"];
+$Experiencia2 = $_POST["Experiencia2"];
+$Nivel2 = $_POST["Nivel2"];
+$Estudia2 = $_POST["Estudia2"];
+$Cursa2 = $_POST["Cursa2"];
+$Institucion2 = $_POST["Institucion2"];
+$Localizacion2 = $_POST["Localizacion2"];
+$Español2 = $_POST["Español2"];
+$Ingles2 = $_POST["Ingles2"];
+$Frances2 = $_POST["Frances2"];
+$Portugues2 = $_POST["Portugues2"];
+$Idiomas2 = $_POST["Idiomas2"];
+$Ocupacion2 = $_POST["Ocupacion2"];
+
+$Contenido ="Foto" . $Foto . "\nNombre" . $Nombre . "\nApellido" . $Apellido . "\nCedula" . $Cedula . "\nNumero" . $Numero . "\nEdad" . $Edad . "\Ciudad" . $Ciudad . "\nSexo" . $sexo . "\nSexualidad" . $sexualidad . "\nExperiencia" . $Experiencia . "\nNivel Educativo" . $Nivel . "\nEstudia actualmente" . $Cursa . "\nQue estudia?" . $Estudia . "\nInstitucion" . $Institucion . "\nLocalizacion" . $Localizacion . "\nidiomas" . $Español . " " . $Ingles . " " . $Frances . " " . $Portugues . " " . $Idiomas . "\nMotivaciones" . $Ocupacion . "\nPareja" . $Pareja . "Datos de la pareja" . "\nFoto" . $Foto2 . "\nNombre" . $Nombre2 . "\nApellido" . $Apellido2 . "\nCedula" . $Cedula2 . "\nNumero" . $Numero2 . "\nEdad" . $Edad2 . "\Ciudad" . $Ciudad2 . "\nSexo" . $sexo2 . "\nSexualidad" . $sexualidad2 . "\nExperiencia" . $Experiencia2 . "\nNivel Educativo" . $Nivel2 . "\nEstudia actualmente" . $Cursa2 . "\nQue estudia?" . $Estudia2 . "\nInstitucion" . $Institucion2 . "\nLocalizacion" . $Localizacion2 . "\nidiomas" . $Español2 . " " . $Ingles2 . " " . $Frances2 . " " . $Portugues2 . " " . $Idiomas2 . "\nMotivaciones" . $Ocupacion2
+
+
+?>
